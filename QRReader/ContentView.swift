4c968e838a0cc4531b37d8c74e11404cbeeebcde
ywangnon <ywangnon123@gmail.com>
2020-03-19 22:46:33 +0900

    Initial Commit

---
+++ QRReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRReader
+//
+//  Created by Hansub Yoo on 2020/03/19.
+//  Copyright © 2020 Hansub Yoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
