ab411d03b2aeaceb3579d380737b761cc6efcab7
ywangnon <ywangnon123@gmail.com>
2020-03-24 00:58:10 +0900

    swift UI

--- QRReader/ContentView.swift
+++ QRReader/ContentView.swift
@@ -8,9 +8,37 @@
 
 import SwiftUI
 
+struct QRItem: Identifiable {
+    var id = UUID()
+    var imageName: String
+    var contents: String
+}
+
+var listData: [QRItem] = [
+    QRItem(imageName: "trash.circle.fill", contents: "test1"),
+    QRItem(imageName: "person.2.fill", contents: "test2"),
+    QRItem(imageName: "car.fill", contents: "test3")
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .bottom) {
+            NavigationView {
+                List(listData) { item in
+                    HStack {
+                        Image(systemName: item.imageName)
+                        Text(item.contents)
+                    }
+                }
+                .navigationBarTitle(Text("QR Reader"))
+            }
+            Button(action: {
+                // contents
+            }) {
+                Text("QR")
+            }
+            .border(Color.black)
+        }
     }
 }
 
